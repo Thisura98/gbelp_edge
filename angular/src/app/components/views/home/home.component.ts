aa8ef0b0d5928af1e95f1c71b01581a2f8aff6f0
Thisura98 <thisura1998@gmail.com>
2021-07-10 08:35:41 +0530

    Angular: Deving home page

--- angular/src/app/components/views/home/home.component.ts
+++ angular/src/app/components/views/home/home.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../../../services/user.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: []
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent{
 
