b1f6d6a17112bcdbf8b92bfe7aab0da98046032b
Thisura98 <thisura1998@gmail.com>
2021-07-11 12:54:48 +0530

    Added all groups frontend

--- angular/src/app/components/views/dashboards/common/dashboardgroups/dashboardgroups.component.ts
+++ angular/src/app/components/views/dashboards/common/dashboardgroups/dashboardgroups.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Group{
+  id: number,
+  name: string,
+  members: number,
+  joinlink: string
+}
+
 @Component({
   selector: 'app-dashboardgroups',
   templateUrl: './dashboardgroups.component.html',
   styleUrls: ['./dashboardgroups.component.css']
 })
 export class DashboardgroupsComponent implements OnInit {
+
+  isLoading = true
+  data: Group[] = []
 
   constructor() { }
 
