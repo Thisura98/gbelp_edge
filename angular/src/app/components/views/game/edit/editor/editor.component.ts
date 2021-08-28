71b0fa0555d6831e0d64994eda8027f6d4ef09cb
Thisura98 <thisura1998@gmail.com>
2021-08-28 22:52:59 +0530

    Added Editor Component Base UI

--- angular/src/app/components/views/game/edit/editor/editor.component.ts
+++ angular/src/app/components/views/game/edit/editor/editor.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { getGameSidebarItems } from 'src/app/constants/constants';
+import { DynamicSidebarItem } from 'src/app/components/ui/dynamicsidebar/dynamicsidebar.component';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.css']
+  styleUrls: [
+    './editor.component.css',
+    '../../common/game.commonstyles.css'
+  ]
 })
 export class GameEditorComponents implements OnInit {
 
+  /**
+   * Left sidebar
+   */
+   get sidebarItems(): DynamicSidebarItem[]{
+    return getGameSidebarItems('Editor');
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
