c05b261888c2d95a3593e60e1e4b80c566000364
Thisura98 <thisura1998@gmail.com>
2021-09-15 21:31:52 +0530

    Angular: Groups view UI improvements

--- angular/src/app/components/views/groups/reports/report.component.ts
+++ angular/src/app/components/views/groups/reports/report.component.ts
@@ -1,8 +1,59 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { DynamicSidebarItem } from "src/app/components/ui/dynamicsidebar/dynamicsidebar.component";
+import { getGroupSidebarItems } from "src/app/constants/constants";
+import { ApiService } from "src/app/services/api.service";
+import { DialogService } from "src/app/services/dialog.service";
+import { UserService } from "src/app/services/user.service";
+import { UtilsService } from "src/app/services/utils.service";
+import { UserGroup } from "../../../../../../../commons/src/models/groups";
 
 @Component({
   templateUrl: './report.component.html',
+  styleUrls: [
+    './report.component.css',
+    '../common/group.commonstyles.css'
+  ]
 })
-export class GroupReportsComponent{
+export class GroupReportsComponent implements OnInit{
 
+  get sidebarItems(): DynamicSidebarItem[]{
+    return getGroupSidebarItems('Reports');
+  }
+  groupId: string | undefined;
+  group: UserGroup | undefined;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private userService: UserService,
+    private apiService: ApiService,
+    private dialogService: DialogService,
+    private utilsService: UtilsService
+  ){}
+
+  ngOnInit(){
+    this.userService.routeOutIfLoggedOut();
+    this.activatedRoute.queryParamMap.subscribe(map => {
+      this.groupId = map.get('groupId') ?? undefined;
+      this.loadData();
+    });
+  }
+
+  private loadData(){
+    this.apiService.getGroup(this.groupId!).subscribe(response => {
+      if (!response.success){
+        const msg = response.description;
+        this.dialogService.showDismissable("Data Load Error", msg);
+
+        // Membership error in API.
+        if (response.code == 201)
+          this.router.navigate(['/dashboard']);
+        return;
+      }
+      
+      this.group = response.data;
+    });
+  }
+  
 }
