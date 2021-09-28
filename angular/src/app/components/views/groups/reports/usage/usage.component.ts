4f2afd5df1d9bf35d6b649c102b8bb3aec35267d
Thisura98 <thisura1998@gmail.com>
2021-09-28 23:59:24 +0530

    Angular: Added usage report component

---
+++ angular/src/app/components/views/groups/reports/usage/usage.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  templateUrl: './usage.component.html'
+})
+export class GroupReportsUsageComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+
+}
