b9361caa51fdc42a5f48b96ea3e301c93750b643
Thisura98 <thisura1998@gmail.com>
2022-07-25 03:21:08 +0530

    Merge changes for properties

--- angular/src/app/components/views/game/edit/editor/properties/properties.component.ts
+++ angular/src/app/components/views/game/edit/editor/properties/properties.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { EditorDataService } from 'src/app/services/editor.data.service';
+import { GameListing } from '../../../../../../../../../commons/src/models/game/game';
 import { EDGEMonacoEditorOptions } from './monaco.editor.options';
 
 type Editor = monaco.editor.IStandaloneCodeEditor;
@@ -15,19 +17,41 @@ type Editor = monaco.editor.IStandaloneCodeEditor;
 export class PropertiesEditorComponent implements OnInit {
   code: string = '[]';
   editorOptions = EDGEMonacoEditorOptions;
+  gameListing: GameListing | undefined;
+
+  userHidEditor: boolean = false;
+  userHidPreview: boolean = true;
+
+  get isTemplate(): boolean{
+    return this.gameListing?.entry.is_template ?? false;
+  }
 
   private editorReference = new BehaviorSubject<Editor | undefined>(undefined);
 
-  constructor() { }
+  constructor(
+    private editorDataService: EditorDataService
+  ) { }
 
   ngOnInit(): void {
+    // Get from Parent Editor Component
+    this.editorDataService.getEditorChildData().subscribe(data => {
+      this.gameListing = data.gameListing?.data;
+    });
   }
 
   /**
    * 'init' callback set on ngx-monaco-editor from template.
    */
-   editorInit(editor: monaco.editor.IStandaloneCodeEditor){
+  editorInit(editor: monaco.editor.IStandaloneCodeEditor){
     this.editorReference.next(editor);;
+  }
+
+  toggleCodeHidden(){
+    this.userHidEditor = !this.userHidEditor
+  }
+
+  togglePreviewHidden(){
+    this.userHidPreview = !this.userHidPreview;
   }
 
 }
