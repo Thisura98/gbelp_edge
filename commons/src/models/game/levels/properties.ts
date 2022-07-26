e4f6fe920e9d081112fba8ae08dd54c11f1aabd4
Thisura98 <thisura1998@gmail.com>
2022-07-27 00:03:10 +0530

    Level Property Model

--- commons/src/models/game/levels/properties.ts
+++ commons/src/models/game/levels/properties.ts
@@ -1,3 +1,54 @@
+export interface NumberProperties{
+    min: number;
+    max: number;
+    defaultNumber: number;
+}
+
+export interface TextProperties{
+    charLimit: number;
+    defaultString: string;
+}
+
+export interface SelectProperties{
+    options: { [key: string] : object };
+}
+
+export interface TemplateProperties{
+    template: LevelProperty;
+    templateIndexer: string;
+    templateRepeatOn: string;
+}
+
+export interface ResourceProperties{
+    resourceType: 'sprite' | 'sound';
+    resourceId: string | null;
+}
+
+export interface LevelPropertyProperties{
+    levelId: string | null;
+}
+
+export interface LevelProperty{
+    id?: string;
+    name: string;
+    hint?: string;
+    type: 'number' | 'text' | 'select' | 'template' | 'break' | 'resource' | 'level';
+
+    number?: NumberProperties;
+    text?: TextProperties;
+    select?: SelectProperties;
+    template?: TemplateProperties;
+    resource?: ResourceProperties;
+    level?: LevelPropertyProperties;
+}
+
+export class LevelPropertySection{
+    constructor(
+        public section: string,
+        public properties: LevelProperty[]
+    ){}
+}
+
 export class LevelProperties{
     constructor(
         public propertyFormHTML: string,
