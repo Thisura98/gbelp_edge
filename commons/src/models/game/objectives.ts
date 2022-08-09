ef83b3ffa0025283a94f5b203039e3b49cbba471
Thisura98 <thisura1998@gmail.com>
2022-08-09 20:48:04 +0530

    Create new models for tracking progress

--- commons/src/models/game/objectives.ts
+++ commons/src/models/game/objectives.ts
@@ -21,3 +21,31 @@ export class GameObjective implements IGameObjective{
 
     }
 }
+
+export class ProgressfulGameObjective extends GameObjective{
+    constructor(
+        public objective_id: number | undefined,
+        public game_entry_id: number | undefined,
+        public name: string,
+        public description: string | undefined,
+        public max_value: number,
+        public progress: number
+    ){
+        super(objective_id, game_entry_id, name, description, max_value);
+    }
+
+    /**
+     * Initialize a progressful objective from an ordinary one.
+     * Default hit is zero.
+     */
+    public static from(objective: GameObjective, withProgress: number = 0): ProgressfulGameObjective{
+        return new ProgressfulGameObjective(
+            objective.objective_id,
+            objective.game_entry_id,
+            objective.name,
+            objective.description,
+            objective.max_value,
+            withProgress
+        )
+    }
+}
--- commons/src/models/game/trackers.ts
+++ commons/src/models/game/trackers.ts
@@ -19,3 +19,34 @@ export class GameGuidanceTracker implements IGameGuidanceTracker{
         public max_threshold: number
     ){}
 }
+
+/**
+ * Used as ViewModel for Splay component. Parent class is `GameGuidanceTracker`
+ */
+export class ProgressfulGameGuidanceTracker extends GameGuidanceTracker{
+    constructor(
+        public tracker_id: number | undefined,
+        public game_entry_id: number | undefined,
+        public name: string,
+        public message: string | undefined,
+        public max_threshold: number,
+        public hits: number
+    ){
+        super(tracker_id, game_entry_id, name, message, max_threshold)
+    }
+
+    /**
+     * Initialize a progressful guidance tracker from an ordinary one.
+     * Default hit is zero.
+     */
+    public static from(tracker: GameGuidanceTracker, withProgress: number = 0): ProgressfulGameGuidanceTracker{
+        return new ProgressfulGameGuidanceTracker(
+            tracker.tracker_id,
+            tracker.game_entry_id,
+            tracker.name,
+            tracker.message,
+            tracker.max_threshold,
+            withProgress
+        );
+    }
+}
