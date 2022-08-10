fd4dd2603779e9d8dd603cbd56c0ca39e79904bb
Thisura98 <thisura1998@gmail.com>
2022-08-10 07:40:19 +0530

    Added EdgeInternals interface

---
+++ node/src/gamelib/edgeinternals.interface.ts
@@ -0,0 +1,8 @@
+/**
+ * TS interface for `window.EdgeInternals` 
+ * loaded in the singleplayer.lib.js Game Lib file.
+ */
+export interface IEdgeInternals{
+    _on_updateObjective: (name: string, progress: number) => void;
+    _on_updateGuidance: (name: string, progress: number) => void;
+}
