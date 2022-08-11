8e4af0ddf3dbaf7d13161c25ff2b18e6a284a66f
Thisura98 <thisura1998@gmail.com>
2022-08-11 19:13:06 +0530

    [Angular] Minor change

--- angular/src/app/components/ui/groups/session-data-table/groups.sessiontable.adapter.ts
+++ angular/src/app/components/ui/groups/session-data-table/groups.sessiontable.adapter.ts
@@ -149,10 +149,8 @@ export class GroupsSessionDataAdapter{
     else if (tomorrow.getTime() > date.getTime() && date.getTime() > today.getTime())
       return 'Tomorrow';
 
-    // todo
-    // return Tomorrow, Yesterday
     const month = (date.getMonth() + 1)!.toString().padStart(2, '0');
-    const day = (date.getDate())!.toString().padEnd(2, '0');
+    const day = (date.getDate())!.toString().padStart(2, '0');
 
     return `${date.getFullYear()}-${month}-${day}`;
   }
