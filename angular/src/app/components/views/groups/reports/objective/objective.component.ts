288f6a64e547cc59a749a65b52e4f19be16975f2
Thisura98 <thisura1998@gmail.com>
2022-08-23 22:11:21 +0530

    [Angular] Load basic data in Objective Reports

--- angular/src/app/components/views/groups/reports/objective/objective.component.ts
+++ angular/src/app/components/views/groups/reports/objective/objective.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { forkJoin } from "rxjs";
 import { DynamicSidebarItem } from "src/app/components/ui/dynamicsidebar/dynamicsidebar.component";
 import { ApiService } from "src/app/services/api.service";
 import { DialogService } from "src/app/services/dialog.service";
@@ -45,7 +46,7 @@ export class GroupReportsObjectiveComponent{
     this.activatedRoute.queryParamMap.subscribe(map => {
       this.groupId = map.get('groupId') ?? undefined;
       this.sessionId = map.get('sessionId') ?? undefined;
-      // this.loadData();
+      this.loadData();
     });
   }
 
@@ -58,6 +59,50 @@ export class GroupReportsObjectiveComponent{
         sessionId: this.sessionId!
       }
     })
+  }
+
+  private loadData() {
+    forkJoin([
+      this.apiService.session.getSession(this.sessionId!),
+      this.apiService.group.getGroup(this.groupId!),
+    ]).subscribe(values => {
+
+      const sessionResponse = values[0];
+      const groupResponse = values[1];
+
+      // Check Membership errors.
+      if (!groupResponse.success) {
+        const msg = groupResponse.description;
+        this.dialogService.showDismissable("Data Load Error", msg);
+        if (groupResponse.code == 201)
+          this.router.navigate(['/dashboard/f/groups']);
+        return;
+      }
+
+      // Check session errors.
+      if (!sessionResponse.success) {
+        const msg = sessionResponse.description;
+        this.dialogService.showDismissable("Session Load Error", msg);
+        return;
+      }
+
+      this.group = groupResponse.data;
+      this.session = sessionResponse.data;
+
+      this.apiService.game.getGame(this.session.game_entry_id).subscribe(res => {
+        if (res.success){
+          this.gameListing = res.data;
+          // this.loadReportData();
+        }
+        else{
+          this.showGameLoadError(res.description);
+        }
+      }, err => this.showGameLoadError(String(err)))
+    })
+  }
+
+  private showGameLoadError(err: string){
+    this.dialogService.showDismissable('Failed to load Game', err);
   }
 
 }
