8cbcd846f5f6d18a2aced581dd04908100aaefed
Thisura98 <thisura1998@gmail.com>
2022-09-26 01:02:50 +0530

    [Angular] Updated test for Game Resources component

--- angular/src/app/components/views/game/edit/resources/resources.component.spec.ts
+++ angular/src/app/components/views/game/edit/resources/resources.component.spec.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from "@angular/common";
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, fakeAsync, TestBed, tick } from "@angular/core/testing";
 import { MatDialog } from "@angular/material/dialog";
-import { ActivatedRoute, Router, RouterModule } from "@angular/router";
+import { By } from "@angular/platform-browser";
+import { ActivatedRoute, Data, Router, RouterModule } from "@angular/router";
+import { ServerResponse } from "src/app/models/common-models";
 import { ToastrService } from "ngx-toastr";
 import { Observable } from "rxjs";
 import { ServerResponseGameListing, ServerResponseGameProject } from "src/app/models/game/game";
+import { ResourceUrlTransformPipe } from "src/app/pipes/resource-url-transform.pipe";
 import { ApiService } from "src/app/services/api.service";
+import { GameEntryAPIs } from "src/app/services/apis/game-entry.api";
 import { DialogService } from "src/app/services/dialog.service";
 import { GameEntry, GameListing, GameType } from "../../../../../../../../commons/src/models/game/game";
 import { GameProject } from "../../../../../../../../commons/src/models/game/project";
+import { GameProjectResource, GameResourceType } from "../../../../../../../../commons/src/models/game/resources";
 import { GameEditResourcesComponent } from "./resources.component";
+import { ViewMode } from "src/app/constants/constants";
 
 type Params = {
   [key: string]: any;
@@ -29,6 +35,10 @@ function createGameEntry(name: string): GameEntry{
   );
 }
 
+function createGameResource(type: GameResourceType): GameProjectResource{
+  return new GameProjectResource('', '', '', type)
+}
+
 function createGameProject(): GameProject{
   return new GameProject('', [], []);
 }
@@ -37,16 +47,22 @@ function createGameListing(name: string): GameListing{
   return { project: createGameProject(), entry: createGameEntry(name) };
 }
 
+function createResponse<T>(data: T): ServerResponse<T>{
+  return {
+    success: true,
+    code: 200,
+    description: '',
+    data: data
+  };
+}
+
 class MockAPIService{
-  game = {
+  getFileSystemBaseURL = () => { return '' };
+
+  game = { 
     getGame: (gameId: string | number): Observable<ServerResponseGameListing> => {
       return new Observable<ServerResponseGameListing>(s => {
-        s.next({
-          success: true,
-          code: 200,
-          description: '',
-          data: createGameListing('Test Game')
-        });
+        s.next();
       });
     }
   };
@@ -54,12 +70,7 @@ class MockAPIService{
   editor = {
     uploadGameResource: (data: FormData, progressCallback: (progress: number) => void): Observable<ServerResponseGameProject> => {
       return new Observable<ServerResponseGameProject>(s => {
-        s.next({
-          success: true,
-          code: 200,
-          description: '',
-          data: createGameProject()
-        })
+        s.next(createResponse(createGameProject()))
       });
     }
   }
@@ -69,18 +80,21 @@ describe('GameEditResourcesComponent', () => {
   let component: GameEditResourcesComponent;
   let fixture: ComponentFixture<GameEditResourcesComponent>;
   let dialogServiceSpy: jasmine.SpyObj<DialogService>
+  let apiService: ApiService;
 
   beforeEach(async () => {
     dialogServiceSpy = jasmine.createSpyObj<DialogService>('DialogService', ['showDismissable']);
 
     let activatedRoute = {
       queryParams: new Observable<Params>(s => s.next({ gameId: 1 })),
-      
+      data: new Observable<Data>(s => s.next({ mode: ViewMode.GAME }))
     };
 
     await TestBed.configureTestingModule({
-      declarations: [GameEditResourcesComponent],
+
+      declarations: [ ResourceUrlTransformPipe, GameEditResourcesComponent ],
       providers: [
+        ResourceUrlTransformPipe,
         { provide: ActivatedRoute, useValue: activatedRoute },
         { provide: Router, useValue: jasmine.createSpyObj('Router', ['navigate']) },
         { provide: ApiService, useClass: MockAPIService },
@@ -93,9 +107,39 @@ describe('GameEditResourcesComponent', () => {
 
     fixture = TestBed.createComponent(GameEditResourcesComponent);
     component = fixture.componentInstance;
+    apiService = TestBed.inject(ApiService);
   })
 
-  it('test', () => {
-
+  it('should create', () => {
+    expect(component).toBeDefined();
   });
+
+  it('should load 2 images and 3 sounds', fakeAsync(() => {
+    let spy = spyOn(apiService.game, 'getGame');
+    let resources: GameProjectResource[] = [
+      createGameResource(GameResourceType.IMAGE),
+      createGameResource(GameResourceType.IMAGE),
+      createGameResource(GameResourceType.SOUND),
+      createGameResource(GameResourceType.SOUND),
+      createGameResource(GameResourceType.SOUND),
+    ];
+    let project: GameProject = new GameProject('0', resources, []);
+    let listing: GameListing = { project: project, entry: createGameEntry('') };
+    let response = createResponse(listing);
+    spy.and.returnValue(new Observable<ServerResponseGameListing>(s => s.next(response)));
+
+    fixture.detectChanges();
+
+    tick();
+
+    fixture.detectChanges();
+
+    let imageElements = fixture.debugElement.queryAll(By.css('.asset-img-container')).length;
+    let soundElements = fixture.debugElement.queryAll(By.css('.asset-snd-container')).length;
+    
+    expect(spy).toHaveBeenCalled();
+    expect(imageElements).toEqual(2);
+    expect(soundElements).toEqual(3);
+
+  }));
 });
