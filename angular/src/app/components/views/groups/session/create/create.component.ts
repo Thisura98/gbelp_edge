468574c5904da68a1fa8185a4ad8feabbc2d7c29
Thisura98 <thisura1998@gmail.com>
2022-11-23 08:24:51 +0530

    [Angular] Show games list

--- angular/src/app/components/views/groups/session/create/create.component.ts
+++ angular/src/app/components/views/groups/session/create/create.component.ts
@@ -1,7 +1,12 @@
 import { Location } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { DynamicSidebarItem } from "src/app/components/ui/dynamicsidebar/dynamicsidebar.component";
+import { ServerResponseAllGameEntries } from "src/app/models/game/game";
+import { ApiService } from "src/app/services/api.service";
+import { DialogService } from "src/app/services/dialog.service";
+import { UserService } from "src/app/services/user.service";
+import { GameEntry } from "../../../../../../../../commons/src/models/game/game";
 
 @Component({
   selector: 'app-group-session-create',
@@ -11,7 +16,7 @@ import { DynamicSidebarItem } from "src/app/components/ui/dynamicsidebar/dynamic
     '../../common/group.commonstyles.css'
   ]
 })
-export class GroupSessionCreateComponent{
+export class GroupSessionCreateComponent implements OnInit{
 
   isEditMode = false;
   title = (this.isEditMode ? 'Edit ' : 'Create ') + 'Session';
@@ -22,13 +27,22 @@ export class GroupSessionCreateComponent{
   endDateAndTime = "";
   sessionScheduleSummary = "The quick brow fox jumped over the lazy dog";
 
+  games: GameEntry[] | undefined;
+
   get sidebarItems(): DynamicSidebarItem[]{
     return [];
   }
 
   constructor(
-    private location: Location
+    private location: Location,
+    private userService: UserService,
+    private apiService: ApiService,
+    private dialogService: DialogService
   ){}
+
+  ngOnInit(){
+    this.loadData();
+  }
 
   handleBack(){
     this.location.back();
@@ -40,6 +54,36 @@ export class GroupSessionCreateComponent{
 
   cancelButtonClicked(){
     this.handleBack();
+  }
+
+  private loadData(){
+    this.userService.routeOutIfLoggedOut();
+    let userId = this.userService.getUserAndToken().user?.userId
+
+    if (userId == null){
+      return;
+    }
+
+    this.apiService.game.getAllGames(false, userId!).subscribe(
+      res => this.handleGamesResponse(res), 
+      (error) => this.handleError(error)
+    );
+  }
+
+  private handleGamesResponse(data: ServerResponseAllGameEntries | null){
+    
+    if (data == null){
+      this.handleError('Games list could not be loaded');
+      return;
+    }
+    
+    this.games = data.data;
+  }
+
+  private handleError(data: any){
+    const title = 'Error Occurred';
+    const msg = typeof(data) === 'string' ? data : String(data).toString();
+    this.dialogService.showDismissable(title, msg);
   }
 
 }
